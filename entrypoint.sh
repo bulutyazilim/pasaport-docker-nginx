ca4f9f7af999dd2a3cd8e528c5c9a7fa76698ab7
İsmail BASKIN <ismailbaskin1@gmail.com>
2016-12-25 00:11:29 +0300

    Remove kong dependency

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,14 +1,7 @@
 #!/bin/sh
 
 if [ -z "$PHP_PORT" ]; then PHP_PORT="9000"; fi
-if [ -z "$KONG_PORT" ]; then KONG_PORT="8000"; fi
-if [ -z "$KONG_ADMIN_PORT" ]; then KONG_ADMIN_PORT="8001"; fi
-if [ -z "$PG_PORT" ]; then PG_PORT="5432"; fi
-
-if [ -n "$KONG_HOST" ]; then export KONG_HOST=$(nslookup $KONG_HOST | awk '/^Address 1: / { print $3 }'); fi
-if [ -n "$PG_HOST" ]; then export PG_HOST=$(nslookup $PG_HOST | awk '/^Address 1: / { print $3 }'); fi
 if [ -n "$PHP_HOST" ]; then export PHP_HOST=$(nslookup $PHP_HOST | awk '/^Address 1: / { print $3 }'); fi
-if [ -n "$KONG_ADMIN_HOST" ]; then export KONG_ADMIN_HOST=$(nslookup $KONG_ADMIN_HOST | awk '/^Address 1: / { print $3 }'); fi
 
 sh ./create_gzip_files.sh >/dev/null 2>&1
 
