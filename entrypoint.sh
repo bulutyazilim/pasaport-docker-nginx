99788cbe8719accf531c63599fd200ca81643725
İsmail BASKIN <ismailbaskin1@gmail.com>
2016-12-25 00:11:52 +0300

    Wait for php service ready

--- entrypoint.sh
+++ entrypoint.sh
@@ -6,6 +6,7 @@ if [ -n "$PHP_HOST" ]; then export PHP_HOST=$(nslookup $PHP_HOST | awk '/^Addres
 sh ./create_gzip_files.sh >/dev/null 2>&1
 
 dockerize \
+-wait tcp://${PHP_HOST}:${PHP_PORT} \
 -template /etc/nginx/conf.d/default.tmpl:/etc/nginx/conf.d/default.conf \
 -template /etc/nginx/snippet/ssl.conf.tmpl:/etc/nginx/snippet/ssl.conf \
 nginx
